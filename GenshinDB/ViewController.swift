c28b58be25c6478de8d3248d9217a5e66549923a
Ihwan <ihwan.id@icloud.com>
2021-05-09 21:28:55 +0700

    create element section

--- GenshinDB/ViewController.swift
+++ GenshinDB/ViewController.swift
@@ -12,7 +12,21 @@ import RxSwift
 class ViewController: UITableViewController {
     
     var characters: [String] = []
+    var weapons: [String] = []
+    var elements: [String] = []
     let disposeBag = DisposeBag()
+    
+    enum HomeItem: Int {
+        case character = 99
+        case weapon = 100
+        case element = 101
+    }
+    
+    var items: [HomeItem] = [
+        .character,
+        .weapon,
+        .element
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,28 +38,61 @@ class ViewController: UITableViewController {
     }
     
     func subscribeData(){
-        genshinProvider.rx.request(.characters).map([String].self).subscribe { (event) in
-            switch event{
-            case .success(let response):
-                self.characters.append(contentsOf: response)
-                
+        let reqCharacter = genshinProvider.rx.request(.characters).map([String].self)
+        let reqWeapon = genshinProvider.rx.request(.weapons).map([String].self)
+        let reqElement = genshinProvider.rx.request(.elements).map([String].self)
+        let observable = Observable.zip(reqCharacter.asObservable(), reqWeapon.asObservable(), reqElement.asObservable()) {
+            return ($0, $1, $2)
+        }
+        
+        observable.subscribe { (event) in
+            switch event {
+            case .completed:
+                print("parsing done")
                 self.tableView.reloadData()
+            case .error(let err):
+                print(err.localizedDescription)
+            case .next(let response):
+                self.characters.append(contentsOf: response.0)
+                self.weapons.append(contentsOf: response.1)
+                self.elements.append(contentsOf: response.2)
                 
-            case .error(let error):
-                print(error)
             }
         }.disposed(by: disposeBag)
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1    }
+        return items.count    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "charcell", for: indexPath) as! CharacterCell
-        cell.collectionView.dataSource = self
-        cell.collectionView.delegate = self
-        cell.collectionView.reloadData()
-        return cell
+        
+        let item = items[indexPath.row]
+        switch item {
+        case .character:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "charcell", for: indexPath) as! CharacterCell
+            cell.collectionView.dataSource = self
+            cell.collectionView.delegate = self
+            cell.collectionView.tag = HomeItem.character.rawValue
+            cell.collectionView.reloadData()
+            return cell
+        case .weapon:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "charcell", for: indexPath) as! CharacterCell
+            cell.collectionView.dataSource = self
+            cell.collectionView.delegate = self
+            cell.collectionView.tag = HomeItem.weapon.rawValue
+            cell.collectionView.reloadData()
+            return cell
+        case .element:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "charcell", for: indexPath) as! CharacterCell
+            cell.collectionView.dataSource = self
+            cell.collectionView.delegate = self
+            cell.collectionView.tag = HomeItem.element.rawValue
+            cell.collectionView.reloadData()
+            
+            return cell
+        }
+        
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -59,18 +106,41 @@ extension ViewController: UICollectionViewDataSource {
         return 1
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return characters.count
+        if let item = HomeItem(rawValue: collectionView.tag) {
+            switch item {
+            case .character:
+                return characters.count
+            case .weapon:
+                return weapons.count
+            case .element:
+                return elements.count
+            }
+        }
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "charcell", for: indexPath) as! CharacterViewCell
-        cell.imageView.kf.setImage(with: URL(string: "https://api.genshin.dev/characters/\(characters[indexPath.row])/icon"))
-        return cell
+        let item = HomeItem(rawValue: collectionView.tag) ?? .character
+        switch item {
+        case .character:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "charcell", for: indexPath) as! CharacterViewCell
+            cell.imageView.kf.setImage(with: URL(string: "https://api.genshin.dev/characters/\(characters[indexPath.row])/icon"))
+            return cell
+        case .weapon:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "charcell", for: indexPath) as! CharacterViewCell
+            cell.imageView.kf.setImage(with: URL(string: "https://api.genshin.dev/weapons/\(weapons[indexPath.row])/icon"))
+            return cell
+        case .element:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "charcell", for: indexPath) as! CharacterViewCell
+            cell.imageView.kf.setImage(with: URL(string: "https://api.genshin.dev/elements/\(elements[indexPath.row])/icon"))
+            return cell
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-         let viewController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "detailCharacter") as! DetailCharacterTableViewController
+        let viewController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "detailCharacter") as! DetailCharacterTableViewController
         navigationController?.pushViewController(viewController, animated: true)
     }
 }
@@ -92,6 +162,7 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 12, left: 20, bottom: 12, right: 20)
     }
+    
 }
 
 class CharacterCell: UITableViewCell {
---
+++ README.md
@@ -0,0 +1,8 @@
+# Genshin DB iOS App
+
+### Screenshots
+![home](Screenshot/home.png "Home Screen")
+
+TODO:  
+- [  ] Create MVVM Pattern
+- [ x ] Using Moya, RxSwift
